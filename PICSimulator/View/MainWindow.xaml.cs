4844140cdd80ae3bb9a4b9f58b9ffe7927d389c9
Mike Schwörer <mailport@mikescher.de>
2014-03-31 12:01:29 +0200

    Design mockup

--- PICSimulator/View/MainWindow.xaml.cs
+++ PICSimulator/View/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PICSimulator
+namespace PICSimulator.View
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
