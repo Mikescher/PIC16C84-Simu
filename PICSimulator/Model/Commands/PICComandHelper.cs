e45b91085d6ebceda14e7cf9956895930451103c
Mike Schwörer <mailport@mikescher.de>
2014-04-02 09:29:51 +0200

    Added RegisterGrid

--- PICSimulator/Model/Commands/PICComandHelper.cs
+++ PICSimulator/Model/Commands/PICComandHelper.cs
@@ -5,15 +5,24 @@ namespace PICSimulator.Model.Commands
 	{
 		public static PICCommand CreateCommand(string sct, uint scl, uint pos, uint cmd)
 		{
-			if (BinaryFormatParser.TryParse(PICCommand_ADDWF.COMMANDCODE, cmd)) return new PICCommand_ADDWF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_ANDWF.COMMANDCODE, cmd)) return new PICCommand_ANDWF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_CLRF.COMMANDCODE, cmd)) return new PICCommand_CLRF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_COMF.COMMANDCODE, cmd)) return new PICCommand_COMF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_DECF.COMMANDCODE, cmd)) return new PICCommand_DECF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_DECFSZ.COMMANDCODE, cmd)) return new PICCommand_DECFSZ(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_INCF.COMMANDCODE, cmd)) return new PICCommand_INCF(sct, scl, pos, cmd);
-            else if (BinaryFormatParser.TryParse(PICCommand_INCFSZ.COMMANDCODE, cmd)) return new PICCommand_INCFSZ(sct, scl, pos, cmd);
-			else return null;
-        }
+			if (BinaryFormatParser.TryParse(PICCommand_ADDWF.COMMANDCODE, cmd))
+				return new PICCommand_ADDWF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_ANDWF.COMMANDCODE, cmd))
+				return new PICCommand_ANDWF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_CLRF.COMMANDCODE, cmd))
+				return new PICCommand_CLRF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_COMF.COMMANDCODE, cmd))
+				return new PICCommand_COMF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_DECF.COMMANDCODE, cmd))
+				return new PICCommand_DECF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_DECFSZ.COMMANDCODE, cmd))
+				return new PICCommand_DECFSZ(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_INCF.COMMANDCODE, cmd))
+				return new PICCommand_INCF(sct, scl, pos, cmd);
+			else if (BinaryFormatParser.TryParse(PICCommand_INCFSZ.COMMANDCODE, cmd))
+				return new PICCommand_INCFSZ(sct, scl, pos, cmd);
+			else
+				return null;
+		}
 	}
 }
---
+++ PICSimulator/View/RegisterGrid.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PICSimulator.View
+{
+	/// <summary>
+	/// Interaction logic for RegisterGrid.xaml
+	/// </summary>
+	public partial class RegisterGrid : UserControl
+	{
+		private const int CELL_COUNT_X = 8;
+		private const int CELL_COUNT_Y = 32;
+
+		private const int CELL_FONT_SIZE = 12;
+
+		public RegisterGrid()
+		{
+			InitializeComponent();
+
+			CreateGrid();
+		}
+
+		private void CreateGrid()
+		{
+			// Definitions
+
+			for (int y = 0; y < CELL_COUNT_Y + 1; y++)
+			{
+				gridMain.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0, GridUnitType.Auto) });
+			}
+
+			for (int x = 0; x < CELL_COUNT_X + 1; x++)
+			{
+				gridMain.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0, GridUnitType.Auto) });
+			}
+
+			// Header
+
+			for (int y = 0; y < CELL_COUNT_Y + 1; y++)
+			{
+				Border b = new Border()
+				{
+					BorderBrush = new SolidColorBrush(Colors.Black),
+					BorderThickness = new Thickness(1)
+				};
+
+				TextBlock t = new TextBlock()
+				{
+					Background = new SolidColorBrush(Colors.Gainsboro),
+					Text = string.Format("{0:X02}", y * CELL_COUNT_X),
+					FontFamily = new FontFamily("Courier New"),
+					FontSize = CELL_FONT_SIZE
+				};
+
+				gridMain.Children.Add(b);
+				Grid.SetRow(b, y + 1);
+				Grid.SetColumn(b, 0);
+
+				b.Child = t;
+			}
+
+			for (int x = 0; x < CELL_COUNT_X + 1; x++)
+			{
+				Border b = new Border()
+				{
+					BorderBrush = new SolidColorBrush(Colors.Black),
+					BorderThickness = new Thickness(0, 0, 1, 1)
+				};
+
+				TextBlock t = new TextBlock()
+				{
+					Background = new SolidColorBrush(Colors.Gainsboro),
+					Text = string.Format("{0:X02}", x),
+					FontFamily = new FontFamily("Courier New"),
+					FontSize = CELL_FONT_SIZE
+				};
+
+				gridMain.Children.Add(b);
+				Grid.SetRow(b, 0);
+				Grid.SetColumn(b, x + 1);
+
+				b.Child = t;
+			}
+
+			// Elements
+
+			for (int x = 0; x < CELL_COUNT_X; x++)
+			{
+				for (int y = 0; y < CELL_COUNT_Y; y++)
+				{
+					Border b = new Border()
+					{
+						BorderBrush = new SolidColorBrush(Colors.Black),
+						BorderThickness = new Thickness(0, 0, 1, 1)
+					};
+
+					TextBox t = new TextBox()
+					{
+						BorderThickness = new Thickness(0),
+						Text = "00",
+						FontFamily = new FontFamily("Courier New"),
+						FontSize = CELL_FONT_SIZE
+					};
+
+					gridMain.Children.Add(b);
+					Grid.SetRow(b, y + 1);
+					Grid.SetColumn(b, x + 1);
+
+					b.Child = t;
+				}
+			}
+		}
+	}
+}
