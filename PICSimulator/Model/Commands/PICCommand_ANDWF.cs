6187289ab403e8daa3b1eeb6fe9c2d0b2a83d950
Apfelkuchen91 <Apfelkuchen91@t-online.de>
2014-04-02 09:00:22 +0200

    ADD ANDWF

---
+++ PICSimulator/Model/Commands/PICCommand_ANDWF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PICSimulator.Model.Commands
+{
+    class PICCommand_ANDWF: PICCommand
+	{
+		public const string COMMANDCODE = "00 0101 dfff fff";
+
+		public PICCommand_ANDWF(string sct, uint scl, uint pos, uint cmd)
+			: base(sct, scl, pos, cmd)
+		{
+
+		}
+	}
+}
