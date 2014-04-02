80e6d8836f536c399e28bb91f13a52d2b3df8821
Apfelkuchen91 <Apfelkuchen91@t-online.de>
2014-04-02 08:56:03 +0200

    PC add

---
+++ PICSimulator/Model/ProgramCounter.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PICSimulator.Model
+{
+    class ProgramCounter
+    {
+        uint programmCounter = 0;
+
+        void inkrement()
+        {
+            programmCounter++;
+        }
+
+        void PCgoto(uint _addr)
+        {
+            programmCounter = _addr;
+        }
+
+        void PCcall(uint _addr)
+        {
+            // Addr auf Stack
+            programmCounter = _addr;
+        }
+
+        void PCreturn()
+        {
+            // Addr. von Stack
+        }
+
+        void reset()
+        {
+            programmCounter = 0;
+        }
+    }
+}
